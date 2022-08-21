5c648bbdf3450ecfa874f5a5987a46f8f9b6ca29
Nahid30 <nahidshuvo30@gmail.com>
2022-08-21 14:04:51 +0600

    banner part done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import banner1 from '../../images/banner1.gif';
+import banner2 from '../../images/banner2.gif';
+import logo from '../../images/logo.png';
 
 const Banner = () => {
     return (
         <div>
-            <h3>this is banner</h3>
+            <div class="hero">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <img src={banner1} class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-4xl font-bold">Welcome to <span className='text-orange-600'>Sun Shine</span></h1>
+                        <p class="py-6">We provide the best courses for you! If you want to develop yourself by learning new skills, we are here for you.</p>
+                        <button class="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ import Services from '../Services/Services';
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            
             <Banner></Banner>
             <Services></Services>
             <Footer></Footer>
